6abda6deb742c8ea101d166a99d4f3a11c75bd27
Hyeunseung <hyeunseung03@gmail.com>
2023-02-04 11:52:22 +0900

    #16 회원가입 기능 구현
    view 관련 클래스 네이밍 변경하였습니다.

--- src/main/java/com/large/ticketsystem/view/controller/ViewController.java
+++ src/main/java/com/large/ticketsystem/view/controller/ViewController.java
@@ -1,18 +1,13 @@
-package com.large.ticketsystem.search.controller;
+package com.large.ticketsystem.view.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 @Slf4j
-public class SearchController {
+public class ViewController {
     @GetMapping("/signup")
     public ModelAndView signupPage() {
         return new ModelAndView("signup");
